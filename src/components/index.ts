a379ff4b87c49b03d347ea6051fd6350f086659c
EC2 Default User <ec2-user@ip-172-31-93-56.ec2.internal>
2026-01-29 01:48:54 +0000

    feat(frontend): add component barrel exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as ImageUpload } from './ImageUpload';
 export { default as GenerationStatus } from './GenerationStatus';
-export { default as ModelViewer } from './ModelViewer';
 export type { GenerationState } from './GenerationStatus';
+export { default as ModelViewer } from './ModelViewer';
